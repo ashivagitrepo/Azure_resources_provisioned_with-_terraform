919286ac43af1efbc5bc12d42d088bcf2f2e5b8b
unknown <ashivakumar688@gamil.com>
2026-09-24 23:23:47 +0530

    Add Terraform configuration

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateazuredevopscicd"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
 }
